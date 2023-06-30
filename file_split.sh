70f655b782d988b5310ffd086c00cae3e19783ff
dmitry <dmitry_cec2@mail.ru>
2023-06-30 19:05:20 +0300

    commit22

--- file_split.sh
+++ file_split.sh
@@ -3,8 +3,8 @@
 splitcount=${1}
 splitnumber=${2}
 
-java -jar jmeter_file_splitter.jar \scripts\UC01\UC01_user.csv ${splitcount} ${splitnumber}
-java -jar jmeter_file_splitter.jar \scripts\UC02\UC01_user.csv ${splitcount} ${splitnumber}
+java -jar jmeter_file_splitter.jar \\scripts\\UC01\\UC01_user.csv ${splitcount} ${splitnumber}
+java -jar jmeter_file_splitter.jar \\scripts\UC02\\UC01_user.csv ${splitcount} ${splitnumber}
 
 
 
