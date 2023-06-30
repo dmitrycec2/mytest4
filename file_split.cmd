d3a5514af2bfb7c98b518e3f0e48cfc016113ecb
dmitry <dmitry_cec2@mail.ru>
2023-06-30 19:01:51 +0300

    commit21

--- file_split.cmd
+++ file_split.cmd
@@ -1,5 +1,5 @@
 @echo off
 
-java -jar jmeter_file_splitter.jar \scripts\UC01\UC01_user.csv 2 2
+java -jar jmeter_file_splitter.jar \scripts\UC01\UC01_user.csv 2 1
 java -jar jmeter_file_splitter.jar \scripts\UC02\UC01_user.csv 2 2
 
